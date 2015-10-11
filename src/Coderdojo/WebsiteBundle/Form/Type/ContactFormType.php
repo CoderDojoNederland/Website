d58b42cf7b346fd9570151a986dde0b5a6449c64
Christian Vermeulen <info@christianvermeulen.net>
2015-10-11 18:08:48 +0200

    added contactform

---
+++ src/Coderdojo/WebsiteBundle/Form/Type/ContactFormType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Coderdojo\WebsiteBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+
+/**
+ * Class ContactFormType
+ */
+class ContactFormType extends AbstractType
+{
+    /**
+     * Build the form
+     *
+     * @param FormBuilderInterface $builder form builder
+     * @param array                $options form options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('naam', 'text', array(
+                'attr' => array(
+                    'pattern'     => '.{2,}'
+                )
+            ))
+            ->add('email', 'email')
+            ->add('message', 'textarea', array(
+                'label' => 'Bericht',
+                'attr' => array(
+                    'cols' => 90,
+                    'rows' => 10
+                )
+            ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $collectionConstraint = new Collection(array(
+            'naam' => array(
+                new NotBlank(array('message' => 'Name should not be blank.')),
+                new Length(array('min' => 2))
+            ),
+            'email' => array(
+                new NotBlank(array('message' => 'Email should not be blank.')),
+                new Email(array('message' => 'Invalid email address.'))
+            ),
+            'message' => array(
+                new NotBlank(array('message' => 'Message should not be blank.')),
+                new Length(array('min' => 5))
+            )
+        ));
+
+        $resolver->setDefaults(array(
+            'constraints' => $collectionConstraint
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'contact';
+    }
+}
