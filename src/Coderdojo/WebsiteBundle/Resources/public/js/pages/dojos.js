bbdee69089b35244cabda79fea8f8d8ed7bc89e6
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2015-10-12 22:25:18 +0200

    Fullscreen map and switch between upcoming and all dojos

--- src/Coderdojo/WebsiteBundle/Resources/public/js/pages/dojos.js
+++ src/Coderdojo/WebsiteBundle/Resources/public/js/pages/dojos.js
@@ -1,30 +1,67 @@
-var map;
-function initialize() {
-  var mapCanvas = document.getElementById('all-dojos-map');
-  var mapOptions = {
-    center: new google.maps.LatLng(44.5403, -78.5463),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+(function(){
+
+  var map;
+  function initialize() {
+    var mapCanvas = document.getElementById('all-dojos-map');
+    var mapOptions = {
+      center: new google.maps.LatLng(52.132633, 5.291266),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+      streetViewControl: false,
+      mapTypeControl: false
+    }
+    map = new google.maps.Map(mapCanvas, mapOptions)
+
   }
-  map = new google.maps.Map(mapCanvas, mapOptions)
-
-  console.log('setting up map');
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 
-// on hover, fetch dojo details
-$('.dojo-row').hover(function(e){
-  dojoId = $(this).data('dojo-id');
+  // on hover, fetch dojo details
+  $('.dojo-row').hover(function(e){
+    dojoId = $(this).data('dojo-id');
 
-  // grab dojo object
-  dojo = _dojos[dojoId]
+    // grab dojo object
+    dojo = _dojos[dojoId]
 
-  console.log(dojo);
+    console.log(dojo);
 
-  // focus map on dojo address
+    // focus map on dojo address
+
+  });
+
+  $(function(){
+    // grab reference to the tabs
+    var allDojosTab           = $('[data-js-ref=all-dojos]')
+        upcomingDojosTab      = $('[data-js-ref=upcoming-dojos]'),
+        upcomingDojosContent  = $('[data-js-ref=list-upcoming-dojos]'),
+        allDojosContent       = $('[data-js-ref=list-all-dojos]');
+
+    // when all tab is clicked, switch to all content
+    allDojosTab.on('click', function(e){
+      e.preventDefault();
+
+      // toggle active state of tab
+      allDojosTab.addClass('active');
+      upcomingDojosTab.removeClass('active');
+
+      // toggle visibility of content
+      allDojosContent.removeClass('hidden');
+      upcomingDojosContent.addClass('hidden');
+     
+    });
+
+    upcomingDojosTab.on('click', function(e){
+      e.preventDefault();
+
+      allDojosTab.removeClass('active');
+      upcomingDojosTab.addClass('active')
+
+      allDojosContent.addClass('hidden');
+      upcomingDojosContent.removeClass('hidden');
+    });
 
 
+  });
 
-
-});
+})();
