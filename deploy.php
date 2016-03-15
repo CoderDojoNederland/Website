4cce618525579277c4a2b4d2af7fa2c3b904e61b
Christian Vermeulen <info@christianvermeulen.net>
2016-03-15 20:33:19 +0100

    add cache chmod

--- deploy.php
+++ deploy.php
@@ -11,6 +11,11 @@ server('prod', 'coderdojo.nl', 22)
     ->stage('production')
     ->env('deploy_path', '/deploy-test'); // Define the base path to deploy your project to.
 
+after('deploy', function(){
+    cd('{{release_path}}');
+    run('sudo chmod 777 -R app/cache');
+});
+
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
 // If you're not using a forward agent, then the server has to be able to clone
