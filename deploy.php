620535c6c8b2947d1eb4c905e5154216d85f744a
Christian Vermeulen <info@christianvermeulen.net>
2016-03-16 10:06:45 +0100

    add slack

--- deploy.php
+++ deploy.php
@@ -2,6 +2,15 @@
 
 // All Deployer recipes are based on `recipe/common.php`.
 require 'recipe/symfony.php';
+require 'recipe/slack.php';
+
+set('slack', [
+    'token'       => 'XXX',
+    'team'        => 'CoderDojoNederland',
+    'channel'     => '#website-nl',
+    'username'    => 'DojoBot',
+    'icon'        => 'coderdojo'
+]);
 
 // Define a server for deployment.
 // Let's name it "prod" and use port 22.
@@ -9,7 +18,7 @@ server('prod', 'coderdojo.nl', 22)
     ->user('seer')
     ->forwardAgent() // You can use identity key, ssh config, or username/password to auth on the server.
     ->stage('production')
-    ->env('deploy_path', '/deploy-test'); // Define the base path to deploy your project to.
+    ->env('deploy_path', '/site'); // Define the base path to deploy your project to.
 
 task('set_cache_rights', function(){
     cd('{{release_path}}');
@@ -17,6 +26,7 @@ task('set_cache_rights', function(){
 });
 
 after('deploy', 'set_cache_rights');
+after('deploy', 'deploy:slack');
 
 // Specify the repository from which to download your project's code.
 // The server needs to have git installed for this to work.
