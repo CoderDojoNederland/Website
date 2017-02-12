dfff885748f076a333c159d72fa5b73497ba6cd0
Christian Vermeulen <info@christianvermeulen.net>
2017-02-12 14:22:41 +0100

    ✨ Emails on COC created

---
+++ src/CoderDojo/WebsiteBundle/Subscriber/CocRequestCreatedEvent/NotifyChampion.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace CoderDojo\WebsiteBundle\Subscriber\CocRequestCreatedEvent;
+
+use CoderDojo\WebsiteBundle\Entity\CocRequest;
+use CoderDojo\WebsiteBundle\Event\CocRequestCreatedEvent;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Swift_Mailer;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+class NotifyChampion
+{
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var TwigEngine
+     */
+    private $templating;
+
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * NotifySlack constructor.
+     * @param Swift_Mailer $mailer
+     * @param TwigEngine $templating
+     * @param Registry $doctrine
+     */
+    public function __construct(
+        Swift_Mailer $mailer,
+        TwigEngine $templating,
+        Registry $doctrine
+    ) {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+        $this->doctrine = $doctrine;
+    }
+
+    public function notify(CocRequestCreatedEvent $event)
+    {
+        /** @var CocRequest $coc */
+        $coc = $this->doctrine->getRepository(CocRequest::class)->find($event->getId());
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject(sprintf('We hebben jouw VOG aanvraag ontvangen'))
+            ->setFrom('contact@coderdojo.nl', 'CoderDojo Nederland')
+            ->setReplyTo('contact@coderdojo.nl')
+            ->setTo($coc->getRequestedBy()->getEmail())
+            ->setCc($coc->getRequestedFor()->getEmail())
+            ->setBcc('chris+vogrequest@coderdojo.nl')
+            ->setContentType('text/html')
+            ->setBody(
+                $this->templating->render(
+                    'CoderDojoWebsiteBundle:Dashboard:Email/Coc/created_champion.html.twig',
+                    array(
+                        'coc' => $coc
+                    )
+                )
+            );
+
+        $this->mailer->send($message);
+    }
+}
---
+++ src/CoderDojo/WebsiteBundle/Subscriber/CocRequestCreatedEvent/NotifyFoundation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace CoderDojo\WebsiteBundle\Subscriber\CocRequestCreatedEvent;
+
+use CoderDojo\WebsiteBundle\Entity\CocRequest;
+use CoderDojo\WebsiteBundle\Event\CocRequestCreatedEvent;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Swift_Mailer;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+class NotifyFoundation
+{
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var TwigEngine
+     */
+    private $templating;
+
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * NotifySlack constructor.
+     * @param Swift_Mailer $mailer
+     * @param TwigEngine $templating
+     * @param Registry $doctrine
+     */
+    public function __construct(
+        Swift_Mailer $mailer,
+        TwigEngine $templating,
+        Registry $doctrine
+    ) {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+        $this->doctrine = $doctrine;
+    }
+
+    public function notify(CocRequestCreatedEvent $event)
+    {
+        /** @var CocRequest $coc */
+        $coc = $this->doctrine->getRepository(CocRequest::class)->find($event->getId());
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject(sprintf('Nieuwe VOG Aanvraag'))
+            ->setFrom('contact@coderdojo.nl', 'CoderDojo Nederland')
+            ->setReplyTo($coc->getRequestedBy()->getEmail())
+            ->setTo('contact@coderdojo.nl')
+            ->setContentType('text/html')
+            ->setBody(
+                $this->templating->render(
+                    'CoderDojoWebsiteBundle:Dashboard:Email/Coc/created_foundation.html.twig',
+                    array(
+                        'coc' => $coc
+                    )
+                )
+            );
+
+        $this->mailer->send($message);
+    }
+}
