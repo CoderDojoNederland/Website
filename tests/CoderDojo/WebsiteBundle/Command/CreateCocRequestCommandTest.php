b88bb40355fd290dccb32ffa620559b641e88d7c
Christian Vermeulen <chris@coderdojo.nl>
2017-04-08 14:18:03 +0200

    Remove birthdate from tests

--- tests/CoderDojo/WebsiteBundle/Command/CreateCocRequestCommandTest.php
+++ tests/CoderDojo/WebsiteBundle/Command/CreateCocRequestCommandTest.php
@@ -9,13 +9,10 @@ class CreateCocRequestCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function it_should_check_properties()
     {
-        $birthDate = new \DateTime('1990-01-01');
-
         $command = new CreateCocRequestCommand(
             'uuid-123',
             'CJ',
             'Vermeulen',
-            $birthDate,
             'christian@email.com',
             'some notes',
             1,
@@ -25,7 +22,6 @@ class CreateCocRequestCommandTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('uuid-123', $command->getId());
         $this->assertSame('CJ', $command->getLetters());
         $this->assertSame('Vermeulen', $command->getName());
-        $this->assertSame($birthDate, $command->getBirthdate());
         $this->assertSame('christian@email.com', $command->getEmail());
         $this->assertSame('some notes', $command->getNotes());
         $this->assertSame(1, $command->getUserId());
