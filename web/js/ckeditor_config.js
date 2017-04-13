26a392a2e9575af5b240183b4db592192c3315e1
Christian Vermeulen <chris@coderdojo.nl>
2017-04-13 18:56:16 +0200

    Remove styles from wysiwyg

--- web/js/ckeditor_config.js
+++ web/js/ckeditor_config.js
@@ -2,7 +2,7 @@ CKEDITOR.editorConfig = function( config ) {
     config.toolbar = [
         { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', '-', 'RemoveFormat' ] },
         { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote' ] },
-        { name: 'styles', items: [ 'Styles', 'Format' ] },
+        { name: 'styles', items: [ 'Format' ] },
         { name: 'clipboard', items: [ 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
         { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
         { name: 'insert', items: [ 'Image', 'HorizontalRule', 'SpecialChar' ] },
