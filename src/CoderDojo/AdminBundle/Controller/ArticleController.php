6c39bdad225f01cb3002ffa84a15cda43c48490e
Christian Vermeulen <chris@coderdojo.nl>
2017-04-14 19:45:10 +0200

    Upload header files

--- src/CoderDojo/AdminBundle/Controller/ArticleController.php
+++ src/CoderDojo/AdminBundle/Controller/ArticleController.php
@@ -10,6 +10,8 @@ use Ramsey\Uuid\Uuid;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -47,11 +49,13 @@ class ArticleController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $image = $this->uploadHeader($form->get('image')->getData());
+
             $article = new Article(
                 Uuid::uuid4()->toString(),
                 $form->get('title')->getData(),
                 $form->get('body')->getData(),
-                'image.png',
+                $image,
                 $this->getDoctrine()->getRepository(Category::class)->find($form->get('category')->getData()),
                 $this->getUser()
             );
@@ -82,6 +86,8 @@ class ArticleController extends Controller
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $image = $this->uploadHeader($editForm->get('image')->getData());
+            $article->setImage($image);
 
             $this->getDoctrine()->getManager()->flush();
 
@@ -144,5 +150,25 @@ class ArticleController extends Controller
         $this->get('session')->getFlashBag()->add('success', 'Het artikel is offline!');
 
         return $this->redirectToRoute('admin_blog_article_index');
+    }
+
+    /**
+     * @param UploadedFile $uploadedFile
+     * @return string
+     */
+    private function uploadHeader(UploadedFile $uploadedFile)
+    {
+        $kernel = $this->get('kernel')->getRootDir();
+        $destination = $kernel . '/../web/articles';
+
+        $filesystem = new Filesystem();
+
+        if(!$filesystem->exists($destination)) {
+            $filesystem->mkdir($destination);
+        }
+
+        $uploadedFile->move($destination, $uploadedFile->getClientOriginalName());
+
+        return $uploadedFile->getClientOriginalName();
     }
 }
