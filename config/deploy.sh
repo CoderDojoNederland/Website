bb3395a9cb546bb104387f8c63783f156758835b
Christian Vermeulen <chris@coderdojo.nl>
2017-04-16 11:09:36 +0200

    keep on going

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,11 +4,16 @@ if [[ $TRAVIS_PULL_REQUEST = "false" ]] && [[ $TRAVIS_BRANCH = 'master' ]]; then
     rvm install 2.2
     rvm use 2.2
 
-    openssl aes-256-cbc -k ${DEPLOY_KEY} -in 'config/coderdojo_deploy_enc_travis' -d -a -out '~/.ssh/coderdojo_deploy'
+    ruby -v
+
+    echo 'decrypting deploy key'
+    openssl aes-256-cbc -k ${DEPLOY_KEY} -in './config/coderdojo_deploy_enc_travis' -d -a -out '~/.ssh/coderdojo_deploy'
     chmod 600 '~/.ssh/coderdojo_deploy'
+    ls -als ~/.ssh
 
+    echo 'preparing Capistrano'
     bundle install
-    bundle exec cap production deploy
 
-    exit 0
+    echo 'Deploying!'
+    bundle exec cap production deploy
 fi
