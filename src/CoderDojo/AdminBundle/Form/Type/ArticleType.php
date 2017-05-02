1aa6d9976a011c2f5b85a7e2d315e8d22072d0b4
Christian Vermeulen <chris@coderdojo.nl>
2017-05-02 19:12:06 +0200

    add image constraints to force quality

--- src/CoderDojo/AdminBundle/Form/Type/ArticleType.php
+++ src/CoderDojo/AdminBundle/Form/Type/ArticleType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
 
 class ArticleType extends AbstractType
 {
@@ -20,7 +21,18 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('slug')
             ->add('body', TextareaType::class)
-            ->add('image', FileType::class, ['required' => false, 'mapped'=>false])
+            ->add('image', FileType::class, [
+                'required' => false,
+                'mapped'=>false,
+                'constraints' => [
+                    new Image([
+                        'minWidth' => 630,
+                        'minHeight' => 1200,
+                        'allowPortrait' => false,
+                        'allowSquare' => false
+                    ])
+                ]
+            ])
             ->add('category', EntityType::class, [
                 'class' => 'CoderDojo\WebsiteBundle\Entity\Category',
                 'choice_label' => 'title',
