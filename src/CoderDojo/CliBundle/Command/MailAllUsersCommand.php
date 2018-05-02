c84a1d01d32a5941f4aad2b5f886a4cea06327c0
Christian Vermeulen <info@christianvermeulen.net>
2018-05-02 17:16:50 +0200

    Fix links in email

--- src/CoderDojo/CliBundle/Command/MailAllUsersCommand.php
+++ src/CoderDojo/CliBundle/Command/MailAllUsersCommand.php
@@ -3,6 +3,7 @@
 namespace CoderDojo\CliBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,13 +14,23 @@ class MailAllUsersCommand extends ContainerAwareCommand
         $this
             ->setName('mailer:users:accept-privacy')
             ->setDescription('Send accept privacy mail to all users')
+            ->addOption('dry-run', 'd')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $repository = $this->getContainer()->get('doctrine')->getRepository('CoderDojoWebsiteBundle:User');
-        $users = $repository->findAll();
+        $users = $repository->findBy([], ['id' => 'ASC']);
+
+        $bar = new ProgressBar($output);
+        $bar->setFormat('%current% of %max%'.PHP_EOL.'[%bar%] %percent:3s%%'.PHP_EOL.'%message% '.PHP_EOL);
+        $bar->setBarCharacter('<info>></info>');
+        $bar->setProgressCharacter('<comment>|</comment>');
+        $bar->setEmptyBarCharacter('<info>.</info>');
+        $bar->setMessage('Getting started');
+        $bar->setRedrawFrequency(10);
+        $bar->start(count($users));
 
         foreach($users as $user) {
             $message = \Swift_Message::newInstance()
@@ -38,7 +49,13 @@ class MailAllUsersCommand extends ContainerAwareCommand
                     )
                 );
 
-            $this->getContainer()->get('mailer')->send($message);
+            if (!$input->getOption('dry-run')) {
+                $this->getContainer()->get('mailer')->send($message);
+            }
+
+            $bar->advance();
         }
+
+        $bar->finish();
     }
 }
