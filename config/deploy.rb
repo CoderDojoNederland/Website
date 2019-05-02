135547e014108d53a1098fdbda6cbfcd4d6b384d
Christian Vermeulen <info@christianvermeulen.net>
2019-05-02 09:48:40 +0200

    fix shared upload dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "coderdojo-website"
 set :repo_url, "git@github.com:CoderDojoNederland/Website.git"
 
 set :linked_files, ["app/config/parameters.yml"]
-set :linked_dirs, ["var/logs", "web/articles", "web/media/cache"]
+set :linked_dirs, ["var/logs", "web/articles", "web/club-100-avatars", "web/media/cache"]
 
 set :symfony_console_path, "bin/console"
 set :symfony_console_flags, "--no-debug"
