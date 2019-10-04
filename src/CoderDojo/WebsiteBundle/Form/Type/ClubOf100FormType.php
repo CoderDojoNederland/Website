81b31f98eeb2508c506802618b4ba024a11b0ad0
Christian Vermeulen <info@christianvermeulen.net>
2019-10-04 11:08:22 +0200

    Text updates CV100

--- src/CoderDojo/WebsiteBundle/Form/Type/ClubOf100FormType.php
+++ src/CoderDojo/WebsiteBundle/Form/Type/ClubOf100FormType.php
@@ -112,9 +112,9 @@ class ClubOf100FormType extends AbstractType
             'label' => 'Betaling*',
             'required' => true,
             'choices' => [
-                'Jaarlijks €100 (1 juni)' => 'yearly',
-                'Halfjaarlijks €50 (1 april & 1 oktober)' => 'semi-yearly',
-                'Kwartaallijks €25 (1 januari, 1 april, 1 juli, 1 oktober)' => 'quarterly'
+                'Jaarlijks €100' => 'yearly',
+                'Halfjaarlijks €50' => 'semi-yearly',
+                'Kwartaallijks €25' => 'quarterly'
             ],
             'attr' => [
                 'class' => 'form-control'
