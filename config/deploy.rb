35b2869ce1ad1e794c213769b61cb1d32e6a7f0d
Christian Vermeulen <christian.vermeulen@gmail.com>
2023-10-15 16:05:37 +0200

    update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.17.1"
+lock "3.17.3"
 
 set :application, "coderdojo-website"
 set :repo_url, "git@github.com:CoderDojoNederland/Website.git"
